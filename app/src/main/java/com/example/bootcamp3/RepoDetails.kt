9c73066698e3af2faf645f9ce3c4b0fd59fe27d6
keshavagg123 <109938159+keshavagg123@users.noreply.github.com>
2022-08-17 09:39:08 +0530

    RepoDetails

--- app/src/main/java/com/example/bootcamp3/RepoDetails.kt
+++ app/src/main/java/com/example/bootcamp3/RepoDetails.kt
@@ -10,10 +10,13 @@ class RepoDetails : AppCompatActivity() {
         setContentView(R.layout.repo_details)
 
         val repoData = intent.getParcelableExtra<MyDataItem> (Constants.KEY_INTENT_DATA)
-        findViewById<TextView>(R.id.fullName).text = repoData?.archive_url.toString()
-        findViewById<TextView>(R.id.msg1).text = repoData?.id.toString()
-        findViewById<TextView>(R.id.msg2).text = repoData?.commits_url.toString()
-        findViewById<TextView>(R.id.msg3).text = repoData?.downloads_url.toString()
-
+        findViewById<TextView>(R.id.fullName).text = repoData?.full_name.toString()
+        findViewById<TextView>(R.id.msg1).text = "LANGUAGE: " + repoData?.language.toString()
+        findViewById<TextView>(R.id.msg2).text = "FORKS_COUNT: " + repoData?.forks_count.toString()
+        findViewById<TextView>(R.id.msg3).text = "VISIBLITY: " + repoData?.visibility.toString()
+        findViewById<TextView>(R.id.msg4).text = "ARCHIVE URL: " + repoData?.archive_url.toString()
+        findViewById<TextView>(R.id.msg5).text = "DOWNLOADS_URL: " + repoData?.downloads_url.toString()
+        findViewById<TextView>(R.id.msg6).text = "GIT_COMMITS_URL: " + repoData?.git_commits_url.toString()
+        findViewById<TextView>(R.id.msg7).text = "COMMENTS_URL: " + repoData?.comments_url.toString()
     }
 }
